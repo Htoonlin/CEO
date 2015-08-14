d87416d86848e6a7129106eddc6c6bea288d5759
htoonlin <wizard.htoon@gmail.com>
2015-08-14 11:10:17 +0630

    - Moved DSN to local.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,6 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn' => 'mysql:host=localhost;dbname=ceo_dev;port=3306',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
         ),
