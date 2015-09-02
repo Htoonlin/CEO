c50259f0eb0b4bd1cfb4b83f5a61e0f4610d0910
htoonlin <wizard.htoon@gmail.com>
2015-09-02 14:09:21 +0630

    - Fixed Back Button

--- module/Core/src/Core/Helper/View/BackButton.php
+++ module/Core/src/Core/Helper/View/BackButton.php
@@ -9,14 +9,20 @@
 namespace Core\Helper\View;
 
 use Zend\Form\View\Helper\AbstractHelper;
+use Zend\Http\Header\Referer;
+use Zend\Http\Request;
 use Zend\Mvc\Router\RouteMatch;
 use Zend\Stdlib\RequestInterface;
+use Zend\Uri\Http;
 
 class BackButton extends AbstractHelper
 {
+    /**
+     * @var Request
+     */
     protected $request;
     protected $routeMatch;
-    public function __construct(RequestInterface $request, RouteMatch $match){
+    public function __construct(Request $request, RouteMatch $match){
         $this->request = $request;
         $this->routeMatch = $match;
     }
@@ -28,11 +34,36 @@ class BackButton extends AbstractHelper
         return $this->render($text, $icon, $attr);
     }
 
+    private function clean($uri, $char){
+        $pos = strpos($uri, $char);
+        if($pos > 0){
+            return substr($uri, 0, $pos);
+        }
+        return $uri;
+    }
+
+    private function checkUri($uriA, $uriB){
+        //Remove js parameters
+        $uriA = $this->clean($uriA, '#');
+        $uriB = $this->clean($uriB, '#');
+
+        //Remove query
+        $uriA = $this->clean($uriA, '?');
+        $uriB = $this->clean($uriB, '?');
+
+        return (strcmp($uriA, $uriB) == 0);
+    }
+
     public function render($text, $icon = '', array $attr){
         $referer = $this->request->getHeader('Referer');
         if($referer){
             $href = $referer->getUri();
         }else{
+            $href = $this->getView()->url($this->routeMatch->getMatchedRouteName());
+        }
+
+        $currentUri = $this->request->getUriString();
+        if($this->checkUri($currentUri, $href)){
             $href = $this->getView()->url($this->routeMatch->getMatchedRouteName());
         }
 
